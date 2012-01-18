9b20c2bcdb9e2668965bb7099b5a788b1a0804b2
Vincent Foley <vfoley@gmail.com>
2012-01-17 21:37:37 -0500

    added comments to lexer + removed string concatenation in favor or list + list->string

--- scratch/lexer.scm
+++ scratch/lexer.scm
@@ -1,3 +1,10 @@
+;; make-stream : string -> stream
+;;
+;; Takes a string and returns a "stream" on that string.  The stream
+;; supports two operations:
+;; - next: returns the character under the cursor, without moving the cursor.
+;;         Reading past the end of the stream returns the nul character.
+;; - advance: moves to the cursor to the next character.
 (define (make-stream str)
   (let ((current 0)
         (size (string-length str)))
@@ -9,72 +16,70 @@
         ((advance) (set! current (+ current 1)))))))
 
 
-
+;; Definition of lexical elements used in Scheme.  The extended
+;; characters are taken from R5RS:
+;; http://schemers.org/Documents/Standards/R5RS/HTML/r5rs-Z-H-5.html#%_sec_2.1
 (define digit (string->list "0123456789"))
 (define alpha (string->list "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"))
 (define extended (string->list "!$%&*+-./:<=>?@^_~"))
 
+;; char-identifier? : char -> bool
+;;
+;; Is `c` a character used in an identifier?
 (define (char-identifier? c)
   (or (member c digit)
       (member c alpha)
       (member c extended)))
 
 
-(define (tokenize stream)
-  (let ((token
-         (cond
-           ((char=? (stream 'next) #\nul) (consume-eof stream))
 
-           ((char=? (stream 'next) #\()   (consume-open-paren stream))
-
-           ((char=? (stream 'next) #\))   (consume-close-paren stream))
-
-           ((char=? (stream 'next) #\')   (consume-quote stream))
-
-           ((char=? (stream 'next) #\")   (consume-string stream))
-
-           ((char-identifier? (stream 'next))
-            (let ((ident (consume-identifier stream)))
-               (or (lexeme-keyword ident)
-                   (lexeme-numeric ident)
-                   (cons 'ident ident))))
-
-
-           ((char-whitespace? (stream 'next))
-            (begin
-              (consume-whitespace stream)
-              (tokenize stream)))
-
-           (else (error
-                  (string-append "unknown character: " (make-string 1 (stream 'next))))))))
-    token))
-
-
+;; consume-eof : stream -> token
 (define (consume-eof stream)
   (stream 'advance)
   'eof)
 
+;; consume-open-paren : stream -> token
 (define (consume-open-paren stream)
   (stream 'advance)
   'open-paren)
 
+;; consume-close-paren : stream -> token
 (define (consume-close-paren stream)
   (stream 'advance)
   'close-paren)
 
+;; consume-quote : stream -> token
+;; TODO: Should we return quote-symbol or quote like the keyword?
 (define (consume-quote stream)
   (stream 'advance)
   'quote-symbol)
 
+;; consume-identifier : stream -> token
+;;
+;; Read characters, concatenating them into a string, until a
+;; non-identifier character is read.
 (define (consume-identifier stream)
-  (define (loop acc)
+  (define (loop)
     (cond ((char-identifier? (stream 'next))
-           (let ((c (make-string 1 (stream 'next))))
+           (let ((c (stream 'next)))
              (stream 'advance)
-             (loop (string-append acc c))))
-          (else acc)))
-   (loop ""))
+             (cons c (loop))))
+          (else '())))
+   (list->string (loop)))
 
+
+;; consume-whitespace : stream -> ()
+;;
+;; skip whitespace
+(define (consume-whitespace stream)
+  (if (char-whitespace? (stream 'next))
+      (begin
+        (stream 'advance)
+        (consume-whitespace stream))))
+
+
+
+;; lexeme-keyword : string -> token|bool
 (define (lexeme-keyword lexeme)
   (cond
    ((string=? lexeme "define") 'define)
@@ -88,11 +93,101 @@
    (else #f)))
 
 
+;; lexeme-numeric : string -> token|bool
 (define (lexeme-numeric lexeme)
   (let ((n (string->number lexeme)))
-    (if n (cons 'number n) #f)))
+    (if n
+        (cons 'number n)
+        #f)))
 
 
+
+;; consume-string : stream -> token
+(define (consume-string stream)
+  (define (loop escaped?)
+    (cond
+     ((and (not escaped?) (char=? (stream 'next) #\"))
+      (begin
+        (stream 'advance) ; consume closing double-quote
+        '()))
+
+     ((and (not escaped?) (char=? (stream 'next) #\\))
+      (begin
+        (stream 'advance)
+        (loop #t)))
+
+     ((not escaped?)
+      (let ((c (stream 'next)))
+        (stream 'advance)
+        (cons c (loop #f))))
+
+     (escaped?
+      (let ((c (stream 'next)))
+        (stream 'advance)
+        (cons (cond
+               ((char=? c #\") #\")
+               ((char=? c #\n) #\newline)
+               ((char=? c #\t) #\tab)
+               ((char=? c #\\) #\\))
+              (loop #f))))))
+  (stream 'advance) ; consume opening double-quote.
+  (cons 'string (list->string (loop #f))))
+
+
+
+
+
+(define (consume-character stream)
+  (stream 'advance)
+  )
+
+
+;; tokenize : stream -> token
+;;
+;; Consume characters from the stream and return the next token.
+;; Invalid characters raise an error.
+(define (tokenize stream)
+  (let ((token
+         (cond
+           ((char=? (stream 'next) #\nul) (consume-eof stream))
+
+           ((char=? (stream 'next) #\()   (consume-open-paren stream))
+
+           ((char=? (stream 'next) #\))   (consume-close-paren stream))
+
+           ((char=? (stream 'next) #\')   (consume-quote stream))
+
+           ((char=? (stream 'next) #\")   (consume-string stream))
+
+           ((char=? (stream 'next) #\#)   (consume-character stream))
+
+           ;; We'll skip all white space, then call `tokenize`
+           ;; recursively to return the next token.
+           ((char-whitespace? (stream 'next))
+            (begin
+              (consume-whitespace stream)
+              (tokenize stream)))
+
+           ;; Keywords, numbers and identifiers are all read the same way:
+           ;; 1. Characters are read until we no longer have a char-identifier;
+           ;; 2. If the characters read represent a keyword, return a keyword token;
+           ;; 3. If the characters read represent a number, return a number token;
+           ;; 4. Otherwise, return an identifier token.
+           ((char-identifier? (stream 'next))
+            (let ((ident (consume-identifier stream)))
+               (or (lexeme-keyword ident)
+                   (lexeme-numeric ident)
+                   (cons 'ident ident))))
+
+           (else (error
+                  (string-append "unknown character: "
+                                 (make-string 1 (stream 'next))))))))
+    token))
+
+
+;; lex : string -> [token]
+;;
+;; Call `tokenize` until EOF, accumulating tokens into a list.
 (define (lex str)
   (let ((stream (make-stream str)))
     (define (loop)
@@ -101,44 +196,3 @@
          ((eq? token 'eof) '())
          (else (cons token (loop))))))
     (loop)))
-
-
-(define (consume-string stream)
-  (define (loop escaped? acc)
-    (cond
-     ((and (not escaped?) (char=? (stream 'next) #\"))
-      (begin
-        (stream 'advance) ; consume closing double-quote
-        (cons 'string acc)))
-
-     ((and (not escaped?) (char=? (stream 'next) #\\))
-      (begin
-        (stream 'advance)
-        (loop #t acc)))
-
-     ((not escaped?)
-      (let ((c (stream 'next)))
-        (stream 'advance)
-        (loop #f (string-append acc (make-string 1 c)))))
-
-     (escaped?
-      (let ((c (stream 'next)))
-        (stream 'advance)
-        (cond
-          ((char=? c #\") (loop #f (string-append acc (make-string 1 c))))
-          ((char=? c #\n) (loop #f (string-append acc (make-string 1 #\newline))))
-          ((char=? c #\t) (loop #f (string-append acc (make-string 1 #\tab))))
-          ((char=? c #\\) (loop #f (string-append acc (make-string 1 #\\))))
-
-          )))))
-
-  (stream 'advance) ; consume opening double-quote.
-  (loop #f ""))
-
-
-
-(define (consume-whitespace stream)
-  (if (char-whitespace? (stream 'next))
-      (begin
-        (stream 'advance)
-        (consume-whitespace stream))))
