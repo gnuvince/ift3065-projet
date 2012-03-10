7b5d41d7bf86f40155ad3afa2d97c545e4ccab45
Vincent Foley <vfoley@gmail.com>
2012-03-09 23:33:56 -0500

    Added simplification

---
+++ src/ir/simplify.scm
@@ -0,0 +1,108 @@
+;; SINS
+;; IFT3065 - H12
+;; Vincent Foley-Bourgon (FOLV08078309)
+;; Eric Thivierge (THIE09016601)
+
+
+(include "../utils/utilities.scm")
+(include "../ir/match.scm")
+
+(define (binding? expr)
+  (and (pair? expr)
+       (symbol? (car expr))
+       (pair? (cdr expr))
+       (null? (cddr expr))))
+
+(define (simplify expr)
+  (match expr
+    ;; Expand begin into nested lets.
+    ((begin) '())
+    ((begin ,E) (simplify E))
+    ((begin ,E1 . ,Es)
+     (let ((g (gensym)))
+       (simplify
+        `(let ((,g ,(simplify E1)))
+           (begin ,@Es)))))
+
+
+    ;; Let simplifies to nested lambdas.
+    ((let () . ,Es) (simplify `(begin ,@Es)))
+
+    ;; Labeled let.
+    ((let ,label ,Bs . ,Es) when (and (symbol? label)
+                                      (all? binding? Bs))
+     (let ((vars (map car Bs))
+           (exprs (map cadr Bs)))
+       (simplify
+        `(letrec ((,label (lambda (,@vars) ,@(simplify Es))))
+           (,label ,@exprs)))))
+
+
+    ;; Regular let.
+    ((let ,Bs . ,Es) when (all? binding? Bs)
+     (let ((vars (map car Bs))
+           (exprs (map cadr Bs)))
+     `((lambda (,@vars)
+         ,(simplify `(begin ,@Es)))
+       ,@(map simplify exprs))))
+
+
+    ;; Expand let* into nested lets.
+    ((let* () . ,Es) (simplify `(let () ,@Es)))
+    ((let* (,B1 . ,Bs) . ,Es) when (binding? B1)
+     `(let ((,(car B1) ,(simplify (cadr B1))))
+        ,(simplify
+          `(let* (,@Bs)
+             ,@Es))))
+    ((let* . ,_) (error "ill-formed let* expression"))
+
+
+    ;; Expand letrec into a let + mutations.
+    ((letrec () . ,Es) (simplify `(let () ,@Es)))
+    ((letrec ,Bs . ,Es) when (all? binding? Bs)
+     (let ((vars (map car Bs))
+           (exprs (map cadr Bs)))
+       (simplify
+        `(let (,@(map (lambda (v) (list v #f)) vars))
+           ,@(map (lambda (v e) `(set! ,v ,e)) vars exprs)
+           ,@(map simplify Es)))))
+    ((letrec . ,_) (error "ill-formed letrec expression"))
+
+
+
+
+    ;; Cond expressions simplify to a series of nested ifs.
+    ((cond (else . ,Es)) (simplify `(begin ,@Es)))
+
+    ((cond (,condition . ,actions))
+     when (not (eq? condition 'else))
+     `(if ,(simplify condition)
+          ,(simplify `(begin ,@actions))))
+
+    ((cond (,condition . ,actions) . ,other-clauses)
+     when (not (eq? condition 'else))
+     `(if ,(simplify condition)
+          ,(simplify `(begin ,@actions))
+          ,(simplify `(cond ,@other-clauses))))
+
+    ((cond . ,_) (error "ill-formed cond expression"))
+
+
+
+    ;; Case expressions simplify to an if+memv.
+    ((case ,expr (else . ,actions)) (simplify `(begin ,@actions)))
+    ((case ,expr (,cases . ,actions) . ,rest)
+     when (list? cases)
+     (let ((e (gensym)))
+       `(let ((,e ,(simplify expr)))
+          (if (memv ,e ,cases)
+              ,(simplify `(begin ,@actions))
+              ,(simplify `(case ,e ,@rest))))))
+    ((case . ,_) (error "ill-formed case expression"))
+
+
+    ;; Function calls
+    ((,fn . ,args) `(,(simplify fn) ,@(map simplify args)))
+
+    ;; Return all other expressions as is.
+    (,_ expr)))
