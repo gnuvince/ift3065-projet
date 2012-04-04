89f3907b3b6890d838a298c03309dc0c97628b09
eric <thiveri@gmail.com>
2012-04-03 23:37:45 -0400

    C implementation of pair and vector

---
+++ src/backend/primitives.c
@@ -0,0 +1,182 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "primitives.h"
+#include "primitives_utils.h"
+
+void allocByteField( __bytefield__ *f, size_t num, size_t size ) {
+    f->field = (void*)calloc(num, size);
+    f->fieldsize = num*size;
+    f->next = 0;
+}
+
+void* allocBlock( __bytefield__ *f, __WORD__ size ) {
+    void *b;
+    __WORD__ misalign;
+
+    if ((f->next + size) > f->fieldsize) {
+        return NULL;
+    }
+    else {
+        b = (void*)(f->field + f->next);
+        
+        f->next = f->next + size;
+        misalign = (f->next % __WORDSIZE__);
+        if (misalign != 0)
+            f->next = f->next + (__WORDSIZE__ - misalign);
+
+        return b;
+    }
+}
+
+int __pair_p( __BWORD__ v ) {
+    return ((v & __BOX_MASK__) == __PAIR_TYPE__);
+}
+
+int __int_p( __BWORD__ v ) {
+    return ((v & __BOX_MASK__) == __INT_TYPE__);
+}
+
+__BWORD__ __getcar( __pair__ *p ) {
+    return p->car;
+}
+
+__BWORD__ __getcdr( __pair__ *p ) {
+    return p->cdr;
+}
+
+void __setcar( __pair__ *p, __BWORD__ newcar ) {
+    p->car = newcar;
+}
+
+void __setcdr( __pair__ *p, __BWORD__ newcdr ) {
+    p->cdr = newcdr;
+}
+
+__pair__*  __cons( __bytefield__ *f, __BWORD__ car, __BWORD__ cdr ) {
+    __pair__ *newpair = NULL;
+    
+    newpair = (__pair__*)allocBlock(f, __PAIRSIZE__);
+    
+    if (newpair == NULL)
+        return newpair;
+    else {
+        __setcar(newpair, car);
+        __setcdr(newpair, cdr);
+        return newpair;
+    }
+}
+
+__vector__* __vector( __bytefield__ *f, __WORD__ size) {
+    __vector__ *newvector = NULL;
+    newvector = (__vector__*)allocBlock(f, __WORDSIZE__*(size + 1));
+
+    if (newvector == NULL)
+        return NULL;
+    else {
+        newvector->size = size;
+        /* newvector->head = (newvector + 1); */
+        /* printf("newvector[0]: %llu\n", (bword_t)*(newvector->head)); */
+        return newvector;
+    }
+}
+
+__BWORD__ __vector_ref( __vector__* v, __WORD__ ref) {
+    if (v == NULL) {
+        printf("Invalid vector");
+        exit(-1);
+    }
+        
+    if (ref >= 0 && ref < v->size)
+        return (__BWORD__)*(((__BWORD__*)v) + ref + 1);
+    else {
+        printf("Error: Invalid vector index: %llu\n", (bword_t)ref);
+        exit(-1);        
+    }
+}
+
+void __vector_set( __vector__* v, __WORD__ ref, __BWORD__ val) {
+    __BWORD__ *pos;
+    if (v == NULL) {
+        printf("Invalid vector");
+        exit(-1);
+    }
+
+    if (ref >= 0 && ref < v->size)
+        (__BWORD__)*(((__BWORD__*)v) + ref + 1) = val;
+    else {
+        printf("Error: Invalid vector index: %llu\n", (bword_t)ref);
+        exit(-1);
+    }
+}
+
+__BWORD__ __box( __WORD__ v, __WORD__ type ) {
+    switch (type) {
+    case __PAIR_TYPE__:
+        return (v << __PAIR_SHFT__) + __PAIR_TYPE__;
+        
+    case __INT_TYPE__:
+        return (v << __INT_SHFT__) + __INT_TYPE__;
+
+    default:
+        break;
+    }
+}
+
+__WORD__ __unbox( __BWORD__ v ) {
+    switch (v & __BOX_MASK__) {
+    case __PAIR_TYPE__:
+        return (v >> __PAIR_SHFT__);
+
+    case __INT_TYPE__:
+        return (v >> __INT_SHFT__);
+
+    default:
+        break;
+    }
+}
+
+
+int main(void)
+{
+    __bytefield__ f;
+    __pair__ *p1, *p2, *p3;
+
+    
+    allocByteField(&f, 1024, __PAIRSIZE__);
+    if (f.field == NULL)
+        return -1;
+
+    p1 = __cons(&f, 3, 4);
+    p2 = __cons(&f, 5, 6);
+    p3 = __cons(&f, 7, 8);
+
+    if (p1 == NULL || p2 == NULL || p3 == NULL) {
+        printf("Out of memory\n");
+        return -1;
+    }
+    else {
+        print_pair(p1);
+        print_pair(p2);
+        print_pair(p3);
+    }
+    
+    __BWORD__ a = __getcar(p1);
+    __BWORD__ b = __getcdr(p1);
+    printf("a: %llu, b: %llu\n", (bword_t)a, (bword_t)b);
+
+    __vector__ *v = __vector(&f, 10);
+    if (v == NULL)
+        return -1;
+    else {
+        print_vector(v);
+        __vector_set(v, 1, 42);
+        print_bword(__vector_ref(v, 1));
+        print_bword(__vector_ref(v, 10));
+    }
+    
+    /* printf("%llu, %llu \n", (bword_t)__getcar(p), (bword_t)__getcdr(p)); */
+    
+    
+    free(f.field);
+    return 0;
+}
---
+++ src/backend/primitives.h
@@ -0,0 +1,62 @@
+#include <stdint.h>
+
+typedef uint64_t __WORD__;
+typedef __WORD__ __BWORD__;
+#define bword_t uint64_t
+#define __WORDSIZE__ 8
+
+/* 3 bits for boxing */
+#define __BOX_SIZE__  3
+#define __BOX_MASK__  7
+#define __PAIR_TYPE__ 0
+#define __PAIR_SHFT__ 1 
+#define __INT_TYPE__  1
+#define __INT_SHFT__  3
+
+typedef struct {
+    __WORD__ size;
+    /* __BWORD__ *head; */
+} __vector__;
+
+typedef struct {
+    __BWORD__ car;
+    __BWORD__ cdr;
+} __pair__;
+
+#define __PAIRSIZE__ sizeof(__pair__)
+
+typedef struct {
+    void *field;
+    __WORD__ fieldsize;
+    __WORD__ next;
+} __bytefield__;
+
+void allocByteField( __bytefield__ *f, size_t num, size_t size );
+
+void* allocBlock( __bytefield__ *f, __WORD__ size );
+
+int __pair_p( __BWORD__ v );
+
+int __int_p( __BWORD__ v );
+
+__BWORD__ __getcar( __pair__ *p );
+
+__BWORD__ __getcdr( __pair__ *p );
+
+void __setcar( __pair__ *p, __BWORD__ newcar );
+
+void __setcdr( __pair__ *p, __BWORD__ newcdr );
+
+__pair__* __cons( __bytefield__ *f, __BWORD__ car, __BWORD__ cdr );
+
+__vector__* __vector( __bytefield__ *f, __WORD__ size);
+
+__BWORD__ __vector_ref( __vector__* v, __WORD__ ref);
+
+void __vector_set( __vector__* v, __WORD__ ref, __BWORD__ val);
+
+__BWORD__ __box( __WORD__ v, __WORD__ type );
+
+__WORD__ __unbox( __BWORD__ v );
+
+
