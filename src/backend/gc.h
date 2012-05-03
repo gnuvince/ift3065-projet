76b3226b8527eae1b4bafa3135bdd7b00b2b1ec3
Eric Thivierge <thiveri@gmail.com>
2012-05-03 13:02:15 -0400

    Fixed gc: multiple ref to moved obj

--- src/backend/gc.h
+++ src/backend/gc.h
@@ -12,16 +12,17 @@ void gc_run ( __bytefield__ *from, __bytefield__ *to );
 
 __BWORD__ gc_copyObject ( __BWORD__ root, __bytefield__ *from, __bytefield__ *to );
 
-/* void gc_setStatus( __gcstatus__ *s, __WORD__ mask ); */
+void gc_setState ( __BWORD__ obj, __BWORD__ state );
 
-void gc_setFlags ( __BWORD__ obj, __WORD__ flags );
+__BWORD__ gc_getState ( __BWORD__ obj );
 
 void gc_setAlive ( __BWORD__ obj );
 
-void gc_setMoved ( __BWORD__ obj );
+void gc_setMoved ( __BWORD__ obj, __WORD__ dest);
 
 int gc_isAlive ( __BWORD__ obj );
 
 int gc_isMoved ( __BWORD__ obj );
+
 
 #endif
