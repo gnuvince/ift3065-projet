696b57d0dfbaa02c4a649296e3808979cbc350c9
Eric Thivierge <thiveri@gmail.com>
2012-05-04 13:51:02 -0400

    Added root/frameStack

--- src/backend/test_stack.c
+++ src/backend/test_stack.c
@@ -10,116 +10,59 @@
 #include "bytefield_utils.h"
 #include "stack.h"
 
-char *line = "======================================================================================\n";
-__bytefield__ *heap;
-__VAR__ p1;
+int main() {
 
-int f (int x) {
-    return x + 3;
-}
-
-int main(void)
-{
-    __initHeap(_A_(0));
-
-    heap = getHeap();
-    p1 = allocVar();
-
-    printf(line);
-    printf("Only 1 boxint var. Heap should be empty before and after gc:\n");
-    setVar(p1, __boxint(_A_(1), 42));
-    dumpByteField(getHeap());
-    gc_run(getHeap(), getNewHeap());
-    dumpByteField(getHeap());
-    printf(line);
-
-
-    __asm__ __volatile__ (
-             /* restore c-stack esp & ebp */
-             "movl    _ebp_c, %ebp     \n\t"
-             "movl    _esp_c, %esp     \n\t"
-             );
-
+    __BWORD__ v11 = __boxint(_A_(1), 1);
+    __BWORD__ v12 = __boxint(_A_(1), 2);
+    __BWORD__ v13 = __boxint(_A_(1), 3);
+    __BWORD__ v14 = __boxint(_A_(1), 4);
+        
+    __BWORD__ v21 = __boxint(_A_(1), 5);
+    __BWORD__ v22 = __boxint(_A_(1), 6);
+    __BWORD__ v23 = __boxint(_A_(1), 7);
+    __BWORD__ v24 = __boxint(_A_(1), 8);
+        
+    __BWORD__ v31 = __boxint(_A_(1), 9);
+    __BWORD__ v32 = __boxint(_A_(1), 10);
+    __BWORD__ v33 = __boxint(_A_(1), 11);
+    __BWORD__ v34 = __boxint(_A_(1), 12);
+        
+    pushFrame();
+    dumpFrameStack();
+    pushRoot(&v11);
+    pushRoot(&v12);
+    pushRoot(&v13);
+    pushRoot(&v14);
+    dumpRootStack();
+    dumpFrameStack();
     
-    __asm__ (
-
-             );
+    pushFrame();
+    dumpFrameStack();    
+    pushRoot(&v21);
+    pushRoot(&v22);
+    pushRoot(&v23);
+    pushRoot(&v24);
+    dumpRootStack();
+    dumpFrameStack();
     
-
-    printf(line);
-    printf("Only 1 boxpair var. Heap should contain the pair before and after gc:\n");
-    setVar(p1, __cons(_A_(2), __boxint(_A_(1), 3), __boxint(_A_(1), 4)));
-    dumpByteField(getHeap());
-    gc_run(getHeap(), getNewHeap());
-    dumpByteField(getHeap());
-    printf(line);
-
-    printf(line);
-    printf("Only 1 boxpair var but 2 more pairs created. Heap should contain 3 pairs before and 1 after gc:\n");
-    setVar(p1, __cons(_A_(2), __boxint(_A_(1), 5), __boxint(_A_(1), 6)));
-    setVar(p1, __cons(_A_(2), __boxint(_A_(1), 7), __boxint(_A_(1), 8)));
-
-    dumpByteField(getHeap());
-    gc_run(getHeap(), getNewHeap());
-    dumpByteField(getHeap());
-    printf(line);
-
-    printf(line);
-    printf("2 more boxpair vars, 3 new pairs created. Heap should contain 4 pairs before and 3 after gc:\n");
-    __VAR__ p2, p3;
-    p2 = allocVar();
-    p3 = allocVar();
-
-    setVar(p1, __cons(_A_(2), __boxint(_A_(1), 3), __boxint(_A_(1), 4)));
-    setVar(p2, __cons(_A_(2), __boxint(_A_(1), 5), __boxint(_A_(1), 6)));
-    setVar(p3, __cons(_A_(2), __boxint(_A_(1), 7), __boxint(_A_(1), 8)));
-
-    dumpByteField(getHeap());
-    gc_run(getHeap(), getNewHeap());
-    dumpByteField(getHeap());
-    printf(line);
-
-    printf(line);
-    printf("3 vars. p1 == pair; p2 == vector; p3 == string. Heap should contain 5 pairs before and 3 after gc:\n");
-    setVar(p3, __createString("**Variable p3**"));
-    setVar(p2, __createVector(_A_(1), __boxint(_A_(1), 12)));
-
-    dumpByteField(getHeap());
-    gc_run(getHeap(), getNewHeap());
-    dumpByteField(getHeap());
-    printf(line);
-
-    printf(line);
-    printf("Set 3 vars to null. Heap should have 3 objects before and 0 after\n");
-    setVar(p1, __NULL__);
-    setVar(p2, __NULL__);
-    setVar(p3, __NULL__);
-
-    dumpByteField(getHeap());
-    gc_run(getHeap(), getNewHeap());
-    dumpByteField(getHeap());
-    printf(line);
-
-    printf(line);
-    printf("p1 <= vector(3)\n");
-    printf("p1[0] <= string('Allo')\n");
-    printf("p2 <= cons(10, 11)\n");
-    printf("p1[1] <= p2\n");
-    printf("p2 <= cons(5, 6)\n");
-    printf("p3 <= vector(5)\n");
-    setVar(p1, __createVector(_A_(1), __boxint(_A_(1), 3)));
-    __vectorSet(_A_(2), getVar(p1), __boxint(_A_(1), 0), __createString("Allo"));
-    setVar(p2, __cons(_A_(2), __boxint(_A_(1), 10), __boxint(_A_(1), 11)));
-    __vectorSet(_A_(2), getVar(p1), __boxint(_A_(1), 1), getVar(p2));
-    setVar(p2, __cons(_A_(2), __boxint(_A_(1), 5), __boxint(_A_(1), 6)));
-    setVar(p3, __createVector(_A_(1), __boxint(_A_(1), 5)));
-
-    dumpByteField(getHeap());
-    gc_run(getHeap(), getNewHeap());
-    dumpByteField(getHeap());
-    printf(line);
+    pushFrame();
+    dumpFrameStack();
+    pushRoot(&v31);
+    pushRoot(&v32);
+    pushRoot(&v33);
+    pushRoot(&v34);
+    dumpRootStack();
+    dumpFrameStack();
     
-    freeByteField(getHeap());
-    freeStack();
+    popFrame();
+    dumpFrameStack();
+    
+    popFrame();
+    dumpFrameStack();
+    
+    popFrame();
+    dumpFrameStack();
+    dumpRootStack();
+    
     return __SUCCESS__;
 }
